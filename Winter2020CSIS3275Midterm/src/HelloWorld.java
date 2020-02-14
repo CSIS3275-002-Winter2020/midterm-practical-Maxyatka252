a455a4a01bf8526408ed5dc22822b745f4d15b79
Maxyatka252 <sundarevm@student.douglascollege.ca>
2020-02-14 13:54:43 -0800

    Done! Hopefully correct :-)

---
+++ Winter2020CSIS3275Midterm/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	
+	public static void Main(String[] args) {
+		
+		System.out.println("Maksim Sundarev");
+		System.out.println("sundarevm@student.douglascollege.ca");
+	}	
+}
